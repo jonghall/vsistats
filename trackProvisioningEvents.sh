fbceeb298955418b4844c1fc13114745ba8cf59a
jonhall <jonhall@us.ibm.com>
2021-05-14 06:51:20 -0500

    initial

--- trackProvisioningEvents.sh
+++ trackProvisioningEvents.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 (
 flock -x -w 10 200 || exit 1
-cd /root/AFI
-source /root/.virtualenvs/AFI/bin/activate
+cd /directory
+source /root/.virtualenvs/vsistats/bin/activate
 python trackProvisioningEvents.py
 
 ) 200>/var/lock/.trackProvisioningEvents.exclusivelock
