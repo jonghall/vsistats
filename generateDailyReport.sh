2df43dff2bc2b4f76ced9037d6b474ef96976daa
jonhall <jonhall@us.ibm.com>
2021-05-15 20:17:41 -0500

    initial

--- generateDailyReport.sh
+++ generateDailyReport.sh
@@ -2,7 +2,7 @@
 (
 flock -x -w 10 200 || exit 1
 cd /directory
-source /root/.virtualenvs/vsistats/bin/activate
+source /root/vsistats/venv/bin/activate
 python generateDailyReport.py
 
 ) 200>/var/lock/.generateDailyStats.exclusivelock
